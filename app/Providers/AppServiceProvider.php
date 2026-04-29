f732d315deebd4d2b0736341525618934d1f94ee
dinoanthony042-web <dinoanthony042@gmail.com>
2026-04-29 09:00:53 +0100

    https force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        // Force HTTPS in production
-        
+    { if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 }
