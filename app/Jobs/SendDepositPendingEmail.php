5b109f6a003b84a3ddff5f3e452b97a9281f485a
dinoanthony042-web <dinoanthony042@gmail.com>
2026-04-30 11:43:24 +0100

    stil on mail error

--- app/Jobs/SendDepositPendingEmail.php
+++ app/Jobs/SendDepositPendingEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\Queueable;
+use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
